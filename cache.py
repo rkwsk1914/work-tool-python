0161f14fc1c3b09b0f01dd5bd9cf936eb507f752
rkwsk1914 <gettodoithallhaste9@gmail.com>
2022-04-08 12:43:36 +0900

    backlog suport

--- cache.py
+++ cache.py
@@ -2,26 +2,65 @@ import sys
 import asyncio
 import time
 import pprint
+import re
+
 from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
 
 from modules.common.check_param import CheckParam
-from modules.app.cache import Cache
+from modules.common.hearing import Hearing
+from modules.app.cache import Cache, CacheBacklog
 
 
+cc = None
 args = sys.argv
 check_param = CheckParam()
 
-env = check_param.checkEnv(args, 'Please enter merge environment. ex)wiro-br', 1)
-param = check_param.checkFormat(
-    args,
-    'Please enter new cache param.  ex)20220309 20220309_1 20220309-1',
-    2,
-    r'\d{8}(\_\d|\-\d)?',
-    'Incorrect format. Please enter an 8-digit number.  ex)20220309 20220309_1 20220309-1'
-)
+def switchSet():
+    hearinger = Hearing()
+    answer = hearinger.select('Do you want to get the cache measure list from BackLog or SVN?', ['BackLog', 'SVN'], blank_ok=False)
+    if answer == 'BackLog':
+        setBackLog()
+    elif answer == 'SVN':
+        setSVN()
 
-cc = Cache(env, param)
+
+def setSVN():
+    env = check_param.checkEnv(args, 'Please enter merge environment. ', 1)
+    param = check_param.checkFormat(
+        args,
+        'Please enter new cache param.  ex)20220309 20220309_1 20220309-1',
+        2,
+        r'\d{8}(\_\d|\-\d)?',
+        'Incorrect format. Please enter an 8-digit number.  ex)20220309 20220309_1 20220309-1'
+    )
+
+    cc = Cache(env, param)
+    return
+
+
+def setBackLog():
+    comment_url = check_param.checkBackLogUrl(args, 'Please enter BackLog Page URL.', 1)
+    param = check_param.checkFormat(
+        args,
+        'Please enter new cache param.  ex)20220309 20220309_1 20220309-1',
+        2,
+        r'\d{8}(\_\d|\-\d)?',
+        'Incorrect format. Please enter an 8-digit number.  ex)20220309 20220309_1 20220309-1'
+    )
+    cc = CacheBacklog(comment_url, param)
+    return
+
+
+if len(args) > 1:
+    check_backlog = re.search(r'https://sbweb.backlog.jp', args[1])
+    pprint.pprint(check_backlog)
+    if check_backlog is None:
+        setSVN()
+    else:
+        setBackLog()
+else:
+    switchSet()
 
 
 def run_concurrent_origin(num, code_files):
